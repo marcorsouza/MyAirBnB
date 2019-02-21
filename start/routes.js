825b6eb00361cbf617659525c233bbc84d1bb50b
marcorsouza <marco.rsouza@gmail.com>
2019-02-21 16:00:19 -0300

    start project

---
+++ start/routes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Route = use('Route')
+
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+
+Route.post('/sessions', 'SessionController.create')
+
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
